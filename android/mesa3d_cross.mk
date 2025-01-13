0f78ba88d15a339ac6551a8405378f0c1e26bb7d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-13 17:37:39 +0800

    LINEAGE: android: Add prebuilts/mesa-build-dep/bin to $PATH in build recipe
    * Add the repository to lineage.dependencies while at it
    
    Change-Id: Ifee0459db06b28b641eb69dc1bcf33ffe6545f38

--- android/mesa3d_cross.mk
+++ android/mesa3d_cross.mk
@@ -83,7 +83,7 @@ MESA3D_GLES_BINS := \
     $($(M_TARGET_PREFIX)MESA3D_LIBGLESV2_BIN) \
 
 MESON_GEN_NINJA := \
-	cd $(MESON_OUT_DIR) && PATH=/usr/bin:/usr/local/bin:$$PATH meson ./build     \
+	cd $(MESON_OUT_DIR) && PATH=/usr/bin:/usr/local/bin:$(AOSP_ABSOLUTE_PATH)/prebuilts/mesa-build-dep/bin:$$PATH meson ./build \
 	--cross-file $(call relative-to-absolute,$(MESON_GEN_DIR))/aosp_cross        \
 	--buildtype=release                                                          \
 	-Dplatforms=android                                                          \
@@ -99,7 +99,7 @@ MESON_GEN_NINJA := \
 	-Dandroid-libbacktrace=disabled                                              \
 	$(BOARD_MESA3D_MESON_ARGS)                                                   \
 
-MESON_BUILD := PATH=/usr/bin:/bin:/sbin:$$PATH ninja -C $(MESON_OUT_DIR)/build
+MESON_BUILD := PATH=/usr/bin:/bin:/sbin:$(AOSP_ABSOLUTE_PATH)/prebuilts/mesa-build-dep/bin:$$PATH ninja -C $(MESON_OUT_DIR)/build
 
 $(MESON_GEN_FILES_TARGET): MESON_CPU_FAMILY := $(subst arm64,aarch64,$(TARGET_$(M_TARGET_PREFIX)ARCH))
 
